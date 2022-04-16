cf716a95c39986b9f7a181052be103fbd7272e81
PriyankDonda <priyankdonda08@gmail.com>
2022-04-16 23:58:26 +0530

    client (react) added...

--- src/index.js
+++ src/index.js
@@ -10,12 +10,6 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(path.join(__dirname + "../client/build")))
-app.get('*', (req,res)=>{
-    // res.sendFile('index.html', { root: path.join(__dirname, '../client/build') });
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-})
-
 app.use(express.json())
 app.use(cookieparser())
 app.use(adminRouter)
