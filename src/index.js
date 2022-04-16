3218eb4cf5d06ee28c3de3f22480065019b16faf
PriyankDonda <priyankdonda08@gmail.com>
2022-04-16 11:17:52 +0530

    client (react) added...

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const adminRouter = require('./routers/admin')
 const patientRouter = require('./routers/patient')
 const doctorRouter = require('./routers/doctor')
 const cookieparser = require("cookie-parser")
+const path = require('path')
 
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
+
+// app.use(express.static(path.join(__dirname + "/src")))
 
 app.use(express.json())
 app.use(cookieparser())
@@ -16,9 +19,14 @@ app.use(patientRouter)
 app.use(doctorRouter)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('../client/public'))
+    console.log('path : ',__dirname)
+    app.use(express.static('../client/build'))
+    app.get('*',(req,res)=>{
+        req.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    })
 }
 
 app.listen(port, () => {
+    console.log(process.env.MONGODB_URL);
     console.log('server is up on port ' + port)
 })
