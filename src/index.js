db85a8969de5358debc03e8c0a1425e703e37988
PriyankDonda <priyankdonda08@gmail.com>
2022-04-16 11:31:51 +0530

    client (react) added...

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 // app.use(express.static(path.join(__dirname + "/src")))
 
@@ -19,14 +19,13 @@ app.use(patientRouter)
 app.use(doctorRouter)
 
 if(process.env.NODE_ENV === 'production'){
-    console.log('path : ',__dirname)
-    app.use(express.static('../client/build'))
+    app.use(express.static(__dirname,'../client/build'))
     app.get('*',(req,res)=>{
         req.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
     })
 }
 
 app.listen(port, () => {
-    console.log(process.env.MONGODB_URL);
+    console.log('path : ',__dirname)
     console.log('server is up on port ' + port)
 })
